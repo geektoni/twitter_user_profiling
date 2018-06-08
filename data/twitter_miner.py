1b8ae129a129746aab3f62479dd009cde4a8af5a
Giovanni De Toni <giovanni.det@gmail.com>
2018-06-08 12:32:57 +0200

    [twitter_miner] Refactor the code and improved usability.
    The utility now will generate a new file each time it finishes to upload
    the data to S3. The previous file is also removed from the system.

--- data/twitter_miner.py
+++ data/twitter_miner.py
@@ -23,7 +23,8 @@ from tweepy.streaming import StreamListener
 from stop_words import get_stop_words
 
 import gzip
-import json
+
+import boto3
 
 # Parse the command line
 arguments= docopt(__doc__)
@@ -37,40 +38,68 @@ access_token = os.environ["ACCESS_TOKEN"]
 access_secret = os.environ["ACCESS_SECRET"]
 
 # Get the authentication handler
+print("[*] Logging in into Twitter API")
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 # Get the API handler
 api = tp.API(auth)
 
-# Generate file name
-timestr = time.strftime("%Y%m%d-%H%M%S")
-filename = "twitter-"+timestr+".json.gz"
-
 # Get italian stop-words (this is done to overcome the weakness of the
 # free Twitter API which do not permit to filter tweets for language only)
 it_stop_words = get_stop_words("it")
 
+# Open Amazon S3 connection to save the dataset
+print("[*] Logging into Amazon S3")
+aws_s3 = boto3.client('s3')
+bucket_name = "twitter100days"
+
 # Custom listener to filter the Twitter stream
 class TwitterFilter(StreamListener):
 
+	def __init__(self):
+		start_time = time.strftime("%Y%m%d-%H%M%S")
+		self.filename = "twitter-" + start_time
+		self.tweets=0
+		super(TwitterFilter, self).__init__(StreamListener)
+
+
+	# Custom method to check file size and upload it to amazon s3
+	def check_size(self):
+		size = os.path.getsize(self.filename)
+		if size >= 104857600:  # 100 MB
+			end_time = time.strftime("%H%M%S")
+			print("[*] Uploading the datafile to Amazon S3.")
+			aws_s3.upload_file(self.filename, bucket_name,  self.filename + "-" + end_time + ".json.gz")
+			print("[*] Generate new filename.")
+			start_time = time.strftime("%Y%m%d-%H%M%S")
+			self.tweets=0
+			os.remove(self.filename)
+			return "twitter-" + start_time
+		return self.filename
+
 	def on_data(self, raw_data):
 		try:
-			with gzip.GzipFile(filename, "ab") as log:
-				print("[*] Writing tweet")
+			if self.tweets%10 == 0:
+				print("[*] "+str(self.tweets)+": Writing tweet")
+			with gzip.GzipFile(self.filename, "ab") as log:
 				log.write(raw_data.encode())
-				return True
+			self.tweets += 1
+			self.filename = self.check_size()
+			return True
 		except BaseException as e:
 			print(str(e))
 
 	def on_error(self, status_code):
 		print("Error "+ str(status_code))
 		# If we get rate limited we close the connection
-		if (status_code == 420):
+		if status_code == 420:
 			return False
 		return True
 
+
 # Get the stream
+print("[*] Get Twitter Stream and start listerning.")
 twitter_stream = Stream(auth, TwitterFilter())
 twitter_stream.filter(track=it_stop_words, languages=['it'])
 
